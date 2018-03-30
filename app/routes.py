730c0f09d0323844031c71c34427d7c7d6343cf7
Kevin Lewis <kevin@jellybabi.es>
2018-03-30 12:14:08 +0100

    Changing hard coded links to url_for()

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -34,5 +34,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect('/')
+        return redirect(url_for('index'))
     return render_template("login.html", form=form)
