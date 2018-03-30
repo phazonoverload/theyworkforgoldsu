3c4d4c1cb7c07545300f68080950d3d399a4e297
Kevin Lewis <kevin@jellybabi.es>
2018-03-30 13:01:43 +0100

    Setup User DB Model

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from app import db
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(128), index=True, unique=True)
+  email = db.Column(db.String(128), index=True, unique=True)
+  role = db.Column(db.String(64), index=True, unique=True)
+  resigned = db.Column(db.Boolean(), index=True)
+  password_hash = db.Column(db.String(128))
+
+  def __repr__(self):
+    return '<User {}>'.format(self.username)
