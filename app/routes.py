692794eb08fae86a7ebd6ad8d16089a67ebc4f47
Kevin Lewis <kevin@jellybabi.es>
2018-03-30 12:09:18 +0100

    Flash messages for form

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -29,7 +29,10 @@ def officers():
     user = users[0]
     return render_template('officer.html', user=user)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/')
     return render_template("login.html", form=form)
