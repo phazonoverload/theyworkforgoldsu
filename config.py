6e24796f945e3e81cf080162177a02ae71108b00
Kevin Lewis <kevin@jellybabi.es>
2018-04-10 13:29:48 +0100

    Changes to config

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SECRET_KEY = os.environ['SECRET_KEY'] or os.urandom(24)
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABSE_URL'] or \
     'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    PASSPHRASE = os.environ.get('PASSPHRASE')
-    ADMIN_PASS = os.environ.get('ADMIN_PASS')
+    PASSPHRASE = os.environ['PASSPHRASE']
+    ADMIN_PASS = os.environ['ADMIN_PASS']
