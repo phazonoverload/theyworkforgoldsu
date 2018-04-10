a96562fc244b86aff63d11b12bc8360f76ce0375
Kevin Lewis <kevin@jellybabi.es>
2018-04-10 13:42:16 +0100

    Removed real config file from repo, added example file

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI =  'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    PASSPHRASE = os.environ['PASSPHRASE']
-    ADMIN_PASS = os.environ['ADMIN_PASS']
