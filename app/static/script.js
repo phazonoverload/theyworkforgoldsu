45590e07569ed9ecff9830435c0eb7c2db436851
Kevin Lewis <kevin@jellybabi.es>
2018-04-13 17:30:22 +0100

    Mobile nav menu

--- app/static/script.js
+++ app/static/script.js
@@ -1,3 +1,15 @@
-$(".flashes .dismiss").click((e) => {
+$(".flashes .dismiss").click(e => {
   $(".flashes").remove();
+})
+
+const slideout = new Slideout({
+  'panel': document.getElementById('panel'),
+  'menu': document.getElementById('m-menu'),
+  'padding': 256,
+  'tolerance': 70,
+  'side': 'right'
+});
+
+$(".toggle-button").click(e=> {
+  slideout.toggle();
 })
