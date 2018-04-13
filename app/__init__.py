7c0645a9c264a59b37448ed60aed88c2968fd7cb
Kevin Lewis <kevin@jellybabi.es>
2018-04-13 19:48:57 +0100

    Fixed date formatting with flask-moment #7

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_gravatar import Gravatar
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
 gravatar = Gravatar(app,size=400,rating='g',default='retro',force_default=False,use_ssl=True,base_url=None)
 login = LoginManager(app)
 login.login_view = "login"
